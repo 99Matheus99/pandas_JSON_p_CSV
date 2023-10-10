1da584c07ffb49de0acf1159e159f81da392556e
matheus de oliveira <nubinhopro5@gmail.com>
2023-10-10 19:20:09 -0300

    adicionando comentários necessários

--- conversor_JSON_CSV.py
+++ conversor_JSON_CSV.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from time import sleep
 try:
-    df = pd.read_json('dados.json')
+    df = pd.read_json('dados.json') # abre o arquivo
     print('Arquivo dados.csv encontrado...')
+    sleep(1) # espera 1 segundo
+    print(f'Processando {len(df)} registros...') 
     sleep(1)
-    print(f'Processando {len(df)} registros...')
-    sleep(1)
-    df.to_csv('novos_dados.csv', encoding='utf-8', index=False)
+    df.to_csv('novos_dados.csv', encoding='utf-8', index=False) # converte o arquivo e salva
     sleep(1)
     print('Arquivo novos_dados.cvs gerado com sucesso!')
 except FileNotFoundError:
